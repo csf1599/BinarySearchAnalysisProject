1b670d148bbb55b425cd9c894029b2d1b8528ec9
csf1599 <csfrazier1599@gmail.com>
2024-03-29 11:19:18 -0400

    worked on iterative binary search algorithm and wrote part of the test code, going to keep working this morning but I need a mental break.

--- Binary Search Analysis Project.cpp
+++ Binary Search Analysis Project.cpp
@@ -4,15 +4,35 @@
 
 using namespace std;
 int recursiveBinarySearch(const vector<int>& arr, int target, int left, int right) {
-    if (left > right)
+    if (left > right) {
         return -1;
+    }
     int mid = left + (right - left) / 2;
-    if (arr[mid] == target)
+    if (arr[mid] == target) {
         return mid;
-    if (arr[mid] > target)
+    }
+    if (arr[mid] > target) {
         return recursiveBinarySearch(arr, target, left, mid - 1);
+    }
     else
         return recursiveBinarySearch(arr, target, mid + 1, right);
+}
+int iterativeBinarySearch(const vector<int>& arr, int target) {
+    int left = 0, right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else
+            right = mid - 1;
+    }
+    return -1;
 }
 void printVector(const vector<int>& arr) {
     cout << "Vector contents: ";
@@ -46,5 +66,16 @@ int main() {
         cout << " was not found.";
     }
     cout << endl;
+    cout << "iterative search test" << endl;
+    int j = iterativeBinarySearch(arr, target1);
+    cout << target1;
+    if (j != -1) {
+        cout << " found at location " << j + 1; 
+    }
+    else {
+        cout << " was not found.";
+    }
+    cout << endl;
+    
 	return 0;
 }
